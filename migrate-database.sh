b44144a909d53b5e46f21b36262b867bd8183d82
John McCawley <john.mccawley@thedigitalgarage.io>
2017-04-10 18:03:05 -0600

    Update migrate-database.sh

--- migrate-database.sh
+++ migrate-database.sh
@@ -3,3 +3,6 @@ set -e
 
 echo "Migrating database 'php artisan migrate:refresh --seed --force..."
 php artisan migrate:refresh --seed --force
+
+echo "Seeding database 'php artisan db:seed"
+php artisan db:seed
